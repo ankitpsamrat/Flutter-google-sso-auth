f82a4a31242da65bb69cb4ce819189eefe7e5328
ankitpsamrat <ankit.p.samrat@gmail.com>
2024-04-16 23:45:11 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sso_auth/firebase_options.dart';
+import 'package:google_sso_auth/src/screens/login_screen.dart';
 
 Future<void> main() async {
   await Firebase.initializeApp(
@@ -16,7 +17,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Google Sso Auth',
+      home: LoginScreen(),
     );
   }
 }
