3ac24ff3c7f004722c88738c601ac471ec2757fc
ankitpsamrat <ankit.p.samrat@gmail.com>
2024-04-18 18:12:28 +0530

    added

---
+++ lib/src/screens/home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              child: Image.network(
+                  'https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0='),
+            ),
+            SizedBox(height: 20),
+            Text('Name'),
+            SizedBox(height: 20),
+            Text('Email'),
+          ],
+        ),
+      ),
+    );
+  }
+}
